1c9b19103c0b01f744ecc3886c8ac24a6f9616bf
张择端 <jacksparow@live.com>
2019-02-25 17:26:07 +0800

    add point light

---
+++ PhysicalReactor/Render/PointLight.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Light.h"
+class PointLight :
+	public Light
+{
+public:
+	PointLight();
+	~PointLight();
+
+	/////////////////////point
+	IntensityUnits intensityunits;
+	float attenuationradius;
+	float sourceradius;
+	float softsourceradius;
+	float sourcelengh;
+	float lightfalloffexponent;
+	bool  useInverseSquaredFalloff;
+	bool CastShadowformCinematicobjectonly;
+
+
+	///////////////////////point  Light Profile
+	//IES Texture;
+	float IESIntensityScale;
+	bool IESIntensity;
+	
+
+	////////////////////////////// field shadow
+	float raystartoffsetdepthscale;///P
+	bool RayTraceDistanceFieldshadows;/// P
+	
+};
+
