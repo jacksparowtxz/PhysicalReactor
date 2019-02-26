e71d84ec25444bda0f3f24fce5ce0ff8cb689494
张择端 <jacksparow@live.com>
2019-02-26 17:20:34 +0800

    delete some function

--- PhysicalReactor/MISC/MiscHelper.h
+++ PhysicalReactor/MISC/MiscHelper.h
@@ -70,78 +70,7 @@ namespace PRE
 	//void Sleep(float milliseconds);
 	int GetDigitInPos(int num, int pos);
 	
-	template<typename T>
-	 void RadixSort(T* unorderarray,int offset, int datanum);
 
-
-	template<typename T>
-	 void RadixSort(T* unorderarray, int offset,int datanum)
-	{
-		T* radixArrays[RADIX];
-		for (int i = 0; i < 10; i++)
-		{
-			radixArrays[i] = (T*)malloc(sizeof(T)*(datanum + 1));
-			radixArrays[i][0] = 0;
-		}
-		for (int pos=1;pos<=KEYNUM;pos++)
-		{
-			for (int i = 0; i < datanum; i++)
-			{
-				int num = GetDigitInPos(unorderarray[i + offset], pos);
-				int index = ++radixArrays[num][0];
-				radixArrays[num][index] = unorderarray[i + offset];
-			}
-			for (int i=0,j=0;i<RADIX;i++)
-			{
-				for (int k=1;k<=radixArrays[i][0];k++)
-					unorderarray[offset + j++] = radixArrays[i][k];
-				radixArrays[i][0] = 0;
-			}
-		}
-	}
-
-	template<typename T>
-	void mergeBlocks(T* const pDataArray, int arraylen, const int blockNum, T* const ResultArray);
-
-	template<typename T>
-	void mergeBlocks(T* const pDataArray, int arraylen, const int blockNum, T* const ResultArray)
-	{
-		int blockLen = arraylen / blockNum;
-		int blockIndex[blockNum];
-
-		for (int i=0;i<blockNum;++i)
-		{
-			blockIndex[i] = i*blockLen;
-		}
-		T smallest = 0;
-		for (int i=0;i<arraylen;++i)
-		{
-			for (int j=0;j<blockNum;++j)
-			{
-				if (blockIndex[j]<(j*blockLen+blockLen))
-				{
-					smallest = pDataArray[blockIndex[j]];
-					break;
-				}
-			}
-			for (int j=0;j<blockNum;++j)
-			{
-				if ((blockIndex[j] < (j*blockLen + blockLen)) && (pDataArray[blockIndex[j]] < smallest))
-				{
-					smallest = pDataArray[blockIndex[j]];
-				}
-			}
-			for (int j=0;j<blockNum;++j)
-			{
-				if ((blockIndex[j]<(j*blockLen+blockLen))&&(pDataArray[blockLen[j]]==smallest))
-				{
-					++blockIndex[j];
-					break;
-				}
-			}
-			ResultArray[i] = smallest;
-		}
-	}
 
 	template<typename TO, typename FROM>
 	inline TO union_cast(FROM from)
