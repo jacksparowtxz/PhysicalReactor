399f189706758cc145ad8cee61198718877f3f02
张择端 <jacksparow@live.com>
2019-03-05 17:33:33 +0800

    add load hdri menthod

--- PhysicalReactor/MISC/TextureLoader.cpp
+++ PhysicalReactor/MISC/TextureLoader.cpp
@@ -39,6 +39,40 @@ void TextureLoader::LoadTexture(const string & TexturefileName, Texture2D* LoadM
 		 SAFE_RELEASE(TexResrouce);
 		 free(wfilename);
 	 }
+	 else if (exname == "hdr" || exname == "hdri")
+	 {
+		 const char* szname = TexturefileName.c_str();
+		 int width = 0;
+		 int height = 0;
+		 int channelcount = 0;
+		 unsigned char* data = stbi_load(szname, &width, &height, &channelcount, 4);
+
+		 TextureDesc desc;
+		 desc.ArraySize = 1;
+		 desc.BindFlags = BIND_SHADER_RESOURCE | BIND_UNORDERED_ACCESS;
+		 desc.CPUAccessFlags = 0;
+		 desc.Format = FORMAT_R32G32B32A32_FLOAT;
+		 desc.Height = static_cast<uint32_t>(height);
+		 desc.Width = static_cast<uint32_t>(width);
+		 desc.MipLevels = (UINT)log2(max(width, height));
+		 desc.MiscFlags = 0;
+		 desc.Usage = USAGE_DEFAULT;
+
+		 UINT mipwidth = width;
+		 SubresourceData* InitData = new SubresourceData[desc.MipLevels];
+		 for (UINT mip = 0; mip < desc.MipLevels; ++mip)
+		 {
+			 InitData[mip].pSysMem = data;
+			 InitData[mip].SysMemPitch = static_cast<UINT>(mipwidth*(channelcount));
+			 mipwidth = max(1, mipwidth / 2);
+		 }
+		 LoadMap->RequestIndepentShaderReourcesForMIPs(true);
+		 LoadMap->RequesIndenpentUnorderedAccessResoucesForMips(true);
+		 HRESULT hr = Renderer::GetDevice()->CreateTexture2D(&desc, InitData, &LoadMap);
+		 assert(SUCCEEDED(hr));
+		 stbi_image_free(data);
+		 delete InitData;
+	 }
 	 else
 	 {
 		 const char* szname = TexturefileName.c_str();
